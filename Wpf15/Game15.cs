b37e6b2204de740babbf909a1271be525a10437f
denandserg <denandserg@gmail.com>
2018-11-17 01:22:02 +0200

    Game 15 wpf alfa
    Signed-off-by: denandserg <denandserg@gmail.com>

---
+++ Wpf15/Game15.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf15
+{   
+    enum MoveButton { None, ToLeft, ToRight, ToUp, ToDown}
+    class Game15
+    {
+        
+            private Random rnd = new Random();
+            private int[,] _gameField = new int[4, 4];
+            public int zeroX { get; private set; }
+            public int zeroY { get; private set; }
+
+            public int this[int x, int y]
+            {
+                get => _gameField[y, x];
+            }
+
+            public bool IsWin()
+            {
+                return false;
+            }
+
+            public void zeroUp()
+            {
+                if (zeroY == 3) return;
+
+                _gameField[zeroX, zeroY] = _gameField[zeroX, zeroY + 1];
+                ++zeroY;
+                _gameField[zeroX, zeroY] = 0;
+
+            }
+            public void zeroDown()
+            {
+                if (zeroY == 0) return;
+
+                _gameField[zeroX, zeroY] = _gameField[zeroX, zeroY - 1];
+                --zeroY;
+                _gameField[zeroX, zeroY] = 0;
+            }
+
+            public void zeroLeft()
+            {
+                if (zeroX == 3) return;
+
+                _gameField[zeroX, zeroY] = _gameField[zeroX + 1, zeroY];
+                ++zeroX;
+                _gameField[zeroX, zeroY] = 0;
+            }
+            public void zeroRight()
+            {
+                if (zeroX == 0) return;
+
+                _gameField[zeroX, zeroY] = _gameField[zeroX - 1, zeroY];
+                --zeroX;
+                _gameField[zeroX, zeroY] = 0;
+            }
+
+
+
+            public Game15()
+            {
+               InitGame();
+            }
+
+            public void InitGame()
+            {
+                int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 };
+
+                Shuffle(arr);
+
+                for (int n = 0, i = 0; i < 4; ++i)
+                {
+                    for (int j = 0; j < 4; ++j, ++n)
+                    {
+                        _gameField[j, i] = arr[n];
+                        if (arr[n] == 0)
+                        {
+                            zeroX = j;
+                            zeroY = i;
+                        }
+                    }
+                }
+            }
+        
+            public MoveButton CheckAndGo(int value)
+            {
+                if (zeroX > 0 && _gameField[zeroX - 1, zeroY] == value)
+                {
+                    zeroRight();
+                    return MoveButton.ToRight;
+                }
+                if (zeroX < 3 && _gameField[zeroX + 1, zeroY] == value)
+                {
+                    zeroLeft();
+                    return MoveButton.ToLeft;
+                }
+                if (zeroY > 0 && _gameField[zeroX, zeroY - 1] == value)
+                {
+                    zeroDown();
+                    return MoveButton.ToDown;
+                }
+                if (zeroY < 3 && _gameField[zeroX, zeroY + 1] == value)
+                {
+                    zeroUp();
+                    return MoveButton.ToUp;
+                }
+                return MoveButton.None;
+            }
+
+            private void Shuffle(int[] arr)
+            {
+                for (int i = 0; i < arr.Length; ++i)
+                {
+                    int r = rnd.Next(arr.Length);
+                    int tmp = arr[i];
+                    arr[i] = arr[r];
+                    arr[r] = tmp;
+                }
+            }
+        }
+    }
+
---
+++ Wpf15/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game15 _game = new Game15();
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int n = 0, i = 0; i < 4; ++i)
+            {
+                for (int j = 0; j < 4; ++j)
+                {
+                    int val = _game[i, j];
+                    if (val != 0)
+                    {
+                        Button btn = GameGrid.Children[n++] as Button;
+                        if (btn != null)
+                        {
+                            btn.Content = val;
+                            btn.SetValue(Grid.RowProperty, i);
+                            btn.SetValue(Grid.ColumnProperty, j);
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            //int col=(int)btn.GetValue(Grid.ColumnProperty);
+            //int row = (int)btn.GetValue(Grid.RowProperty);
+
+            if (btn != null)
+            {
+                int val = (int)btn.Content;
+                switch (_game.CheckAndGo(val))
+                {
+                    case MoveButton.ToUp:
+                        ButtonToUp(btn);
+                        break;
+                    case MoveButton.ToDown:
+                        ButtonToDown(btn);
+                        break;
+                    case MoveButton.ToLeft:
+                        ButtonToLeft(btn);
+                        break;
+                    case MoveButton.ToRight:
+                        ButtonToRight(btn);
+                        break;
+
+
+                }
+            }
+
+        }
+
+        private void ButtonToUp(Button btn)
+        {
+            int row = (int)btn.GetValue(Grid.RowProperty);
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation();
+            thicknessAnimation.From = new Thickness(0, 150, 0, 0);
+            thicknessAnimation.To = new Thickness(0, 0, 0, 150);
+            thicknessAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(1000));
+            btn.SetValue(Grid.RowSpanProperty, 2);
+            btn.SetValue(Grid.RowProperty, row - 1);
+            thicknessAnimation.FillBehavior = FillBehavior.Stop;
+            thicknessAnimation.Completed += (o, ea) =>
+            {
+                btn.SetValue(Grid.RowSpanProperty, 1);
+            };
+            btn.BeginAnimation(MarginProperty, thicknessAnimation);
+        }
+
+        private void ButtonToDown(Button btn)
+        {
+            int row = (int)btn.GetValue(Grid.RowProperty);
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation();
+            thicknessAnimation.From = new Thickness(0, 0, 0, 150);
+            thicknessAnimation.To = new Thickness(0, 150, 0, 0);
+            thicknessAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(1000));
+            btn.SetValue(Grid.RowSpanProperty, 2);
+            thicknessAnimation.FillBehavior = FillBehavior.Stop;
+            thicknessAnimation.Completed += (o, ea) =>
+            {
+                btn.SetValue(Grid.RowSpanProperty, 1);
+                btn.SetValue(Grid.RowProperty, row + 1);
+            };
+            btn.BeginAnimation(MarginProperty, thicknessAnimation);
+        }
+
+        private void ButtonToLeft(Button btn)
+        {
+            int col = (int)btn.GetValue(Grid.ColumnProperty);
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation();
+            thicknessAnimation.From = new Thickness(150,0,0,0);
+            thicknessAnimation.To = new Thickness(0, 0, 150, 0);
+            thicknessAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(1000));
+            btn.SetValue(Grid.ColumnSpanProperty, 2);
+            btn.SetValue(Grid.ColumnProperty, col - 1);
+            thicknessAnimation.FillBehavior = FillBehavior.Stop;
+            thicknessAnimation.Completed += (o, ea) =>
+            {
+                btn.SetValue(Grid.ColumnSpanProperty, 1);
+
+            };
+            btn.BeginAnimation(MarginProperty, thicknessAnimation);
+        }
+
+        private void ButtonToRight(Button btn)
+        {
+            int col = (int)btn.GetValue(Grid.ColumnProperty);
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation();
+            thicknessAnimation.From = new Thickness(0, 0, 150, 0);
+            thicknessAnimation.To = new Thickness(150, 0, 0, 0);
+            thicknessAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(1000));
+            btn.SetValue(Grid.ColumnSpanProperty, 2);
+            thicknessAnimation.FillBehavior = FillBehavior.Stop;
+            thicknessAnimation.Completed += (o, ea) =>
+            {
+                btn.SetValue(Grid.ColumnSpanProperty, 1);
+                btn.SetValue(Grid.ColumnProperty, col + 1);
+            };
+            btn.BeginAnimation(MarginProperty, thicknessAnimation);
+        }
+    }
+}
